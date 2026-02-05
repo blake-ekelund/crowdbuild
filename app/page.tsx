503faa61d33c00da289224b7d3dd7b15228f91ea
blake-ekelund <blake.ekelund@workchores.com>
2026-02-05 11:42:14 -0500

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Nav from "@/components/Nav";
-import Footer from "@/components/Footer";
+import Nav from "@/app/components/Nav";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
